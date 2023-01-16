de141c497918d2ca67c4bdbb48a162152cf9c78f
SunnyMoonShine <Sunny.Ladjimi@outlook.com>
2023-01-16 20:11:11 +0100

    incrémentation du produit sur la page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -3,11 +3,34 @@ const url = "http://localhost:3000/api/products";
 
 fetch(url)
 .then(response => response.json())
-.then(json => console.log(json))
+.then(data => {
+    const product = data[0];
 
-//Affichage du produit
-for (let i = 0; i <= url.length; i += 1) {
-    let products = document.createElement(url[i]);
-    console.log(products);
-}
+//Création du bloc parent -> Lien vers le produit
+const newA = document.createElement("a");
+newA.href = "./product.html?id=productId";
+const sectionProducts = document.getElementById("items");
+sectionProducts.appendChild(newA);
 
+console.log(sectionProducts)
+
+//Remplissage des images et infos du produit
+const article = document.createElement("article");
+newA.appendChild(article);
+
+const imgElt = document.createElement("img");
+imgElt.src = product.imageUrl;
+
+const nameElt = document.createElement("h3");
+nameElt.innerText = product.name;
+
+const descriptionElt = document.createElement("p");
+descriptionElt.innerText = product.description;
+
+article.appendChild(imgElt);
+article.appendChild(nameElt);
+article.appendChild(descriptionElt);
+
+
+console.log(data)
+})
