33fb0e50ccd3095d0b0789da0b8af7f7046650c6
SunnyMoonShine <Sunny.Ladjimi@outlook.com>
2023-01-17 09:50:32 +0100

    Boucle d'afffichage des articles

--- front/js/script.js
+++ front/js/script.js
@@ -5,32 +5,37 @@ fetch(url)
 .then(response => response.json())
 .then(data => {
     const product = data[0];
-
-//Création du bloc parent -> Lien vers le produit
-const newA = document.createElement("a");
-newA.href = "./product.html?id=productId";
-const sectionProducts = document.getElementById("items");
-sectionProducts.appendChild(newA);
-
-console.log(sectionProducts)
-
-//Remplissage des images et infos du produit
-const article = document.createElement("article");
-newA.appendChild(article);
-
-const imgElt = document.createElement("img");
-imgElt.src = product.imageUrl;
-
-const nameElt = document.createElement("h3");
-nameElt.innerText = product.name;
-
-const descriptionElt = document.createElement("p");
-descriptionElt.innerText = product.description;
-
-article.appendChild(imgElt);
-article.appendChild(nameElt);
-article.appendChild(descriptionElt);
-
-
-console.log(data)
+    
+    //Boucle d'affichage des produits
+        for (let i = 0; i < data.length; i += 1) {
+    
+    //Création du bloc parent -> Lien vers le produit
+    const newA = document.createElement("a");
+    newA.href = "./product.html?id=productId";
+    const sectionProducts = document.getElementById("items");
+    sectionProducts.appendChild(newA);
+    
+    console.log(sectionProducts)
+    
+    //Remplissage des images et infos du produit
+    const article = document.createElement("article");
+    newA.appendChild(article);
+    
+    const imgElt = document.createElement("img");
+    imgElt.src = data[i].imageUrl;
+    imgElt.setAttribute("alt", product.altTxt);
+    
+    const nameElt = document.createElement("h3");
+    nameElt.innerText = data[i].name;
+    
+    const descriptionElt = document.createElement("p");
+    descriptionElt.innerText = data[i].description;
+    
+    article.appendChild(imgElt);
+    article.appendChild(nameElt);
+    article.appendChild(descriptionElt);
+    
+    
+        console.log(data)
+        }
 })
