d77977cab2361284e53e5afe95e72f0019880f20
SunnyMoonShine <Sunny.Ladjimi@outlook.com>
2023-01-24 14:29:54 +0100

    Refactoring script.js + complete product.js

--- front/js/product.js
+++ front/js/product.js
@@ -2,21 +2,33 @@
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
 const id = urlParams.get("id")
+if (id != null) {
+    let itemPrice
+    let imgUrl 
+    let altText
+    let namePrd
+}
 
 //Get API from server 
 fetch(`http://localhost:3000/api/products/${id}`)
 .then(response => response.json())
-.then(prd => handlePrd(prd))
+.then(prd => getPrd(prd))
 
-
-function handlePrd(prd) {
-    const { altTxt, colors, description, imageUrl, name, price, _id } = prd
-    makeImage(imageUrl, altTxt);
-    makeName(name);
-    makePrice(price);
-    makeDescription(description);
+//Fonction globale regroupant les sous-fonctions/élément
+function getPrd(prd) {
+    const { altTxt, colors, description, imageUrl, name, price } = prd
+    itemPrice = price
+    imgUrl = imageUrl
+    altText = altTxt
+    namePrd = name
+    makeImage(imageUrl, altTxt)
+    makeName(name)
+    makePrice(price)
+    makeDescription(description)
+    makeOption(colors)
 }
 
+//Fonction incrémentation image produit
 function makeImage(imageUrl, altTxt) {
     const prdImage = document.createElement("img")
     prdImage.src = imageUrl
@@ -25,17 +37,71 @@ function makeImage(imageUrl, altTxt) {
     parent.appendChild(prdImage) 
 }
 
+//Fonction incrémentation nom produit
 function makeName(name) {
     const prdName = document.getElementById("title")
     prdName.innerText = name
+    return name
 }
 
+//Fonction incrémentation prix
 function makePrice(price) {
     const prdPrice = document.getElementById("price")
     prdPrice.innerText = price
 }
 
+//Fonction incrémentation description
 function makeDescription(description) {
     const prdDescription = document.getElementById("description")
     prdDescription.innerText = description
 }
+
+//Fonction incrémentation option de couleurs + boucle
+function makeOption(color) {
+    color.forEach(color => {
+        const eltOption = document.createElement("option")
+        eltOption.value = color
+        eltOption.textContent = color
+        const colors = document.querySelector("#colors")
+        colors.appendChild(eltOption)
+    });
+}
+
+const alertM = document.querySelector("#addToCart")
+    if (alertM != null) {
+        alertM.addEventListener("click", getLocalStorageCart)
+}
+
+//Fonction permettant de récupérer les items dans le localStorage
+function getLocalStorageCart() {
+        const quantity = document.querySelector("#quantity").value
+        const color = document.querySelector("#colors").value
+
+        if (isCardIsInvalide(quantity, color)) {return}
+        addCart(color, quantity)
+    }
+    
+//Si quantité null -> Message d'alerte
+function isCardIsInvalide(quantity, color) {
+    if (color.length === 0 || quantity.length === 0 || quantity === "0") {
+        alert("Veuillez remplir tous les champs")
+        return true
+    }
+}
+    
+//Fonction ajout d'un item au panier
+function addCart(color, quantity) {
+    const data = {
+        id: id,
+        name: namePrd,
+        color: color,
+        quantity: Number(quantity),
+        price: Number(itemPrice),
+        imageUrl: imgUrl,
+        altTxt: altText
+    }
+        localStorage.setItem(id, JSON.stringify(data))
+        alert("Votre produit a bien été ajouté à votre panier")
+}    
+
+        
--- front/js/script.js
+++ front/js/script.js
@@ -3,11 +3,11 @@ fetch("http://localhost:3000/api/products")
 .then(response => response.json())
 .then(data => addProducts(data))
 
-
-//Fonction globale regroupant les sous-fonctions pour chaque création d'objet
+//Boucle d'affichage des produits
 function addProducts(data) {
     data.forEach(canape => {
         
+        //Fonction globale regroupant les sous-fonctions pour chaque création d'objet
     const { _id, imageUrl, altTxt, name, description } = canape
         
     const fnctId = makeId(_id)
@@ -23,30 +23,37 @@ function addProducts(data) {
 })
 }
 
+//Fonction ID du produit
 function makeId(id) {
         const linkId = document.createElement("a")
         linkId.href = "./product.html?id=" + id
         return linkId
     }
-    
+
+ //Fonction appendChild du bloc article
     function appendChild(link, article) {
         const parentLinkId = document.querySelector("#items")
         parentLinkId.appendChild(link);
         link.appendChild(article)
     }
-    
+
+//Fonction Image du produit    
     function makeImg(imageUrl, altTxt) {
         const img = document.createElement("img")
         img.src = imageUrl
         img.alt = altTxt
         return img
     }
+
+//fonction nom du produit
     function makeName(name) {
         const h3 = document.createElement("h3")
         h3.textContent = name
         h3.classList.add("productName")
         return h3
     }
+
+//Fonction description du produit
     function makeDesc(desc) {
         const dscr = document.createElement("p")
         dscr.textContent = desc
